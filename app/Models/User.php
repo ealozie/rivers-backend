4742fdaf52b38d8d7e25ce8292ab5ba1ce42a5ec
Nathaniel John <david.nathaniel13@gmail.com>
2023-08-25 07:08:24 +0100

    update panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+//use Filament\Models\Contracts\FilamentUser;
+//use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -50,4 +52,9 @@ class User extends Authenticatable
     {
         return $this->hasOne(TicketAgent::class);
     }
+
+    // public function canAccessPanel(Panel $panel): bool
+    // {
+    //     return str_ends_with($this->email, '@gmail.com') && $this->hasVerifiedEmail();
+    // }
 }
