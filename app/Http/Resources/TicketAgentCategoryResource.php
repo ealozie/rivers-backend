dea871bd28d441536425b4e5cb29e04e83602ec0
Nathaniel David <david.nathaniel13@gmail.com>
2023-09-06 18:55:31 +0100

    update

--- app/Http/Resources/TicketAgentCategoryResource.php
+++ app/Http/Resources/TicketAgentCategoryResource.php
@@ -15,7 +15,7 @@ class TicketAgentCategoryResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
+            'id' => $this->ticket_catogory_id,
             'ticket_category' => $this->ticket_category->category_name,
             'ticket_amount' => $this->ticket_category->amount,
             'discount' => (int) $this->discount,
