ad14a3bce006edb0e7b1b515a7cf257f6a18bcc5
Nathaniel David <david.nathaniel13@gmail.com>
2023-11-23 19:56:56 +0100

    update payment information

--- app/Http/Resources/PaymentResource.php
+++ app/Http/Resources/PaymentResource.php
@@ -14,10 +14,25 @@ class PaymentResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
-        // return [
-        //     'id' => $this->id,
-        //     ''
-        // ];
+        //return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'transaction_id' => $this->transaction_id,
+            'reference_number' => $this->reference_number,
+            'transaction_date' => $this->transaction_date,
+            'payer_name' => $this->payer_name,
+            'merchant_reference' => $this->merchant_reference,
+            'retrieval_reference_number' => $this->retrieval_reference_number,
+            'payer_phone_number' => $this->payer_phone_number,
+            'payer_address' => $this->payer_address,
+            'receipt_number' => $this->receipt_number,
+            'payment_gateway' => $this->payment_gateway,
+            'amount' => $this->amount,
+            'method' => $this->method,
+            'payment_channel' => $this->payment_channel,
+            'bank_name' => $this->bank_name,
+            'deposit_slip_number' => $this->deposit_slip_number,
+            'user' => $this->user->name ?? '',
+        ];
     }
 }
--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -10,4 +10,10 @@ class Payment extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
