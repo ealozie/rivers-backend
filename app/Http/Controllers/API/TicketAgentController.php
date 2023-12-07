86a56032bd509a1a2a8db23cf7dc1bc960f461e7
Nathaniel David <david.nathaniel13@gmail.com>
2023-12-07 12:09:00 +0100

    update changes for ticket

--- app/Http/Controllers/API/TicketAgentController.php
+++ app/Http/Controllers/API/TicketAgentController.php
@@ -11,6 +11,7 @@ use App\Models\TicketAgentCategory;
 use App\Models\TicketAgentWallet;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 /**
  * @tags Ticket Agents Service
--- app/Http/Resources/TicketAgentCategoryResource.php
+++ app/Http/Resources/TicketAgentCategoryResource.php
@@ -19,8 +19,20 @@ class TicketAgentCategoryResource extends JsonResource
             'ticket_category' => $this->ticket_category->category_name,
             'ticket_amount' => (int) $this->ticket_category->amount,
             'discount' => (int) $this->discount,
+            'allow_multiple_ticket_purchase' => $this->check_status($this->ticket_category->allow_multiple_ticket_purchase),
+            'allow_multiple_quantity' => $this->check_status($this->ticket_category->allow_multiple_quantity),
             'status' => $this->status,
             'created_at' => (string) $this->created_at,
         ];
+    }
+
+    public function check_status($value)
+    {
+        if ($value == 1) {
+            return true;
+        } else {
+            return false;
+        }
+        
     }
 }
