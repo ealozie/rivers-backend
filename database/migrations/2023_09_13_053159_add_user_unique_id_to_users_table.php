43f9f9d38987e826f22203f48cae63a0006d879d
Nathaniel David <david.nathaniel13@gmail.com>
2023-12-09 11:08:25 +0100

    update changes

--- database/migrations/2023_09_13_053159_add_user_unique_id_to_users_table.php
+++ database/migrations/2023_09_13_053159_add_user_unique_id_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            // $table->string('unique_id')->after('id')->nullable();
+            $table->string('unique_id')->after('id')->nullable();
         });
     }
 
