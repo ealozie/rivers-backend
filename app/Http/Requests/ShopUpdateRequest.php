7292646587f8287cc842c2059cb6e9578a0af7c1
Nathaniel David <david.nathaniel13@gmail.com>
2024-07-27 16:13:55 +0100

    update shop schema

--- app/Http/Requests/ShopUpdateRequest.php
+++ app/Http/Requests/ShopUpdateRequest.php
@@ -35,7 +35,7 @@ class ShopUpdateRequest extends FormRequest
             'business_category_id' => 'required|exists:business_categories,id',
             'business_sub_category_id' => 'required|exists:business_sub_categories,id',
             'classification_id' => 'required|exists:classifications,id',
-            'user_id' => 'required|exists:users,id',
+            'user_id' => 'required',
             'notes' => 'nullable',
         ];
     }
