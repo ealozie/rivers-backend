55cf3fb5cc8a205921c547fd0e77180e50711e67
Nathaniel David <david.nathaniel13@gmail.com>
2024-08-27 20:19:15 +0100

    update account manager

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -51,7 +51,8 @@ class DatabaseSeeder extends Seeder
             //RoleSeeder::class,
             // DocumentLifeSpanSeeder::class,
             // TollGateCategorySeeder::class,
-            PermissionSeeder::class
+            //PermissionSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,12 +12,19 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        $users = [
-            ['name' => 'Nathaniel', 'email' => 'david.nathaniel13@gmail.com', 'password' => bcrypt('xbba063nath'), 'role' => 'admin'],
-        ];
+        // $users = [
+        //     ['name' => 'Nathaniel', 'email' => 'david.nathaniel13@gmail.com', 'password' => bcrypt('xbba063nath'), 'role' => 'admin'],
+        // ];
+        $users = \App\Models\User;
         foreach ($users as $user) {
-            $user = \App\Models\User::create($user);
-            $user->assignRole('admin');
+            $names = explode('@', $user->email);
+            $other_names = explode('.', $names);
+            if (count($other_names) > 1) {
+                $user->name = ucfirst($other_names[0]);
+            } else {
+                $user->name = ucfirst($names[0]);
+            }
+            $user->save();
         }
     }
 }
