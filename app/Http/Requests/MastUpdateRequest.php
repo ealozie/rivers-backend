c0a028ca9154727332f1f77bd1fb7664cb736c26
Nathaniel David <david.nathaniel13@gmail.com>
2025-03-18 11:37:20 +0100

    update changes for mast

--- app/Http/Requests/MastUpdateRequest.php
+++ app/Http/Requests/MastUpdateRequest.php
@@ -22,7 +22,26 @@ class MastUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "mast_location" => "required|string|in:standalone,property",
+            "property_id" => "nullable|exists:properties,property_id",
+            "state_id" => "nullable|integer|exists:states,id",
+            "local_government_area_id" =>
+                "nullable|exists:local_government_areas,id",
+            "street_name" => "nullable|string",
+            "street_number" => "nullable|string",
+            "city" => "nullable|string",
+            "mast_name" => "nullable|string",
+            "approval_status" => "nullable",
+            "mast_use" => "nullable|string",
+            "owner_id" => "nullable|exists:users,unique_id",
+            "connected_to_power" => "required|boolean",
+            "connected_to_diesel_solar_power_generator" => "required|boolean",
+            "longitude" => "nullable|numeric",
+            "latitude" => "nullable|numeric",
+            "note" => "nullable|string",
+            "pictures" => "nullable|array|min:1|max:3",
+            "pictures.*" =>
+                "nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048",
         ];
     }
 }
