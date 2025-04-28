f2cc9a8a0f1079c06b200d43310afccaf66f75f1
Nathaniel David <david.nathaniel13@gmail.com>
2025-04-28 16:14:14 +0100

    update changes

--- app/Http/Resources/MastListResource.php
+++ app/Http/Resources/MastListResource.php
@@ -26,6 +26,7 @@ class MastListResource extends JsonResource
             'city' => $this->city,
             'mast_name' => $this->mast_name,
             'mast_use' => $this->mast_use,
+            'owner' => $this->owner,
             'approval_status' => $this->approval_status,
             'connected_to_power' => $this->connected_to_power ? true : false,
             'connected_to_diesel_solar_power_generator' => $this->connected_to_diesel_solar_power_generator ? true : false,
